647e78cfbf471b700840b910366a53b4eec55051
achintbio <achint.varma@icahn.mssm.edu>
2019-06-30 19:18:43 -0400

    generalized code for problem 3

--- pe3.py
+++ pe3.py
@@ -4,16 +4,27 @@
 Created on Sun Jun 30 17:47:47 2019
 
 Find the largest prime factor of 600851475143
-or find the inverse of the smallest prime factor of 600851475143 in the range of 600851475143
+
+The code works for any generic n, for powers of 2, the n is reduced to 1 
+hence the largest prime factor would is equal to  2 
+ 
 
 @author: Achint the BIG DAWG!!
 """
-n = 600851475143
-l = 0 
-for i in range(2,n):
-    if (n % i == 0):
-        l = n / i 
-        break;
 
-print(l)
-#8462696833.0
+
+
+n = 600851475143
+i = 2
+
+while i * i <= n:
+    while n % i == 0:
+        n = n / i
+    i = i + 1
+
+if n == 1:
+    n = 2
+print(n)
+
+#6857
+
